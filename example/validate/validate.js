8db14f37e0867e3d5c207d8624832ad848e28a76
scd7896 <scd1212@naver.com>
2022-01-27 01:16:53 +0900

    fix: invalidData string return type 예시 추가

--- example/validate/validate.js
+++ example/validate/validate.js
@@ -13,10 +13,18 @@ function submitValidate(obj) {
 	if (obj.number[0] <= 2) return { name: "number", index: 0, message: "The first number must be greater than 2." };
 	if (obj.number[1] <= 4) return { name: "number", index: 1, message: "The second number must be greater than 4." };
 	if (obj.number[0] >= obj.number[1]) return { name: "number", message: "The first number should be less than the second number." }
+	if (obj.textName.length === 0) return "textName";
 }
 
 function submitInvalid(element, invalidData) {
-	alert(invalidData.message);
+	if (invalidData.message) {
+		alert(invalidData.message);
+	}
+
+	if (invalidData === "textName") {
+		alert("The textName length must be greater than 0.")
+	}
+
 	element.style.border = "1px solid red";
 }
 
