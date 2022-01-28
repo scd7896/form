3a3c1c2fd43a7436e5a196e5b77baabd272fc04b
scd7896 <scd1212@naver.com>
2022-01-29 00:53:34 +0900

    fix: npm types 배포 완료

---
+++ index.d.ts
@@ -0,0 +1,21 @@
+export interface IFailValidateReturnType {
+	name: string;
+	message?: string;
+	index?: number;
+}
+
+export interface ISubmitOptions<T> {
+	isExcuteDefault?: boolean;
+	validate?: (validateParameter: T) => string | undefined | IFailValidateReturnType;
+	onInvalid?: (targetInvalidElement?: HTMLElement, invalidData?: IFailValidateReturnType | string) => void;
+}
+
+export function onSubmit<T>(callback: (arg: T | FormData) => void, options?: ISubmitOptions<T>): any;
+
+type invalidCallback = (failTarget?: HTMLElement, failReason?: "valueMissing" | "valid" | "typeMismatch" | "tooShort" | "tooLong" | "rangeUnderflow" | "rangeOverflow" | "badInput" | "customError" | "patternMismatch") => void;
+
+export interface IInvalidOption {
+	isExcuteDefault?: boolean
+}
+
+export function onInvalid(callback: invalidCallback, options?: IInvalidOption): any;
